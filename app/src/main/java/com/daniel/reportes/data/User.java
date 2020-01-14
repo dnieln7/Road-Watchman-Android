8e6b7a225b739fe454295b35ba23d55e22fb3b08
Daniel Antonio Nolasco Alvarado <dnieln7@gmail.com>
2020-01-14 08:31:43 -0600

    add login with google feature

---
+++ app/src/main/java/com/daniel/reportes/data/User.java
@@ -0,0 +1,66 @@
+package com.daniel.reportes.data;
+
+import java.io.Serializable;
+
+public class User implements Serializable {
+    private int id;
+    private String username;
+    private String email;
+    private String password;
+    private String googleId;
+    private String role;
+
+    public User(String username, String email, String password, String googleId, String role) {
+        this.username = username;
+        this.email = email;
+        this.password = password;
+        this.googleId = googleId;
+        this.role = role;
+    }
+
+    public User(String email, String password, String role) {
+        this.email = email;
+        this.password = password;
+        this.role = role;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getGoogleId() {
+        return googleId;
+    }
+
+    public String getRole() {
+        return role;
+    }
+
+    public void setRole(String role) {
+        this.role = role;
+    }
+}
--- app/src/main/java/com/daniel/reportes/ui/login/Login.java
+++ app/src/main/java/com/daniel/reportes/ui/login/Login.java
@@ -2,6 +2,7 @@ package com.daniel.reportes.ui.login;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
 
@@ -10,7 +11,15 @@ import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
 
 import com.daniel.reportes.R;
+import com.daniel.reportes.data.User;
 import com.daniel.reportes.ui.reportes.Reportes;
+import com.daniel.reportes.ui.signup.SignUp;
+import com.google.android.gms.auth.api.signin.GoogleSignIn;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.auth.api.signin.GoogleSignInClient;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.common.api.ApiException;
+import com.google.android.gms.tasks.Task;
 
 public class Login extends AppCompatActivity {
 
@@ -19,7 +28,7 @@ public class Login extends AppCompatActivity {
     // Objects
 
     // Widgets
-    EditText usernameIn;
+    EditText emailIn;
     EditText passwordIn;
 
     @Override
@@ -30,38 +39,94 @@ public class Login extends AppCompatActivity {
         initWidgets();
     }
 
-    private void initVar() {
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
 
-    }
+        if (requestCode == 100) {
+            Task<GoogleSignInAccount> task = GoogleSignIn.getSignedInAccountFromIntent(data);
 
-    private void initObjects() {
+            try {
+                GoogleSignInAccount account = task.getResult(ApiException.class);
 
+                User user = new User(
+                        account.getEmail(),
+                        account.getId(),
+                        "user"
+                );
+
+                login(user);
+            } catch (ApiException e) {
+                Log.w("Error", "signInResult:failed code = " + e.getStatusCode());
+            }
+        }
     }
 
     private void initWidgets() {
-        usernameIn = findViewById(R.id.usernameIn);
+        emailIn = findViewById(R.id.usernameIn);
         passwordIn = findViewById(R.id.passwordIn);
     }
 
-    private void initListeners() {
+    private void login(User userIn) {
 
+        String token = "";
+        User user = null;
+
+
+        if(user != null && !token.equals("")) {
+            Intent intent = new Intent(getBaseContext(), Reportes.class);
+
+            intent.putExtra("user", user);
+            intent.putExtra("token", token);
+
+            startActivity(intent);
+        }
     }
 
-    public void login(View view) {
-        Intent intent = new Intent(getBaseContext(), Reportes.class);
+    public void loginWithEmail(View view) {
 
-        startActivity(intent);
+        User user = new User(
+                emailIn.getText().toString(),
+                passwordIn.getText().toString(),
+                "user"
+        );
+
+        login(user);
+    }
+
+    public void loginWithGoogle(View view) {
+
+        GoogleSignInAccount account = GoogleSignIn.getLastSignedInAccount(this);
+
+        if (account == null) {
+            GoogleSignInOptions options = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                    .requestProfile()
+                    .requestEmail()
+                    .build();
+
+            GoogleSignInClient signIn = GoogleSignIn.getClient(this, options);
+
+            Intent signInIntent = signIn.getSignInIntent();
+
+            startActivityForResult(signInIntent, 100);
+        }
+        else {
+            User user = new User(
+                    account.getEmail(),
+                    account.getId(),
+                    "user"
+                    );
+
+            login(user);
+        }
     }
 
     public void forgotPassword(View view) {
 
-        if(usernameIn.getText().toString().trim().equals("")) {
-            usernameIn.setError("Requerido!");
+        if (emailIn.getText().toString().trim().equals("")) {
+            emailIn.setError("Requerido!");
         }
         else {
-
-
-
             Fragment mFragment = new ResetPassword();
             FragmentManager fragmentManager = getSupportFragmentManager();
             fragmentManager.beginTransaction().replace(R.id.forgotPassword, mFragment).commit();
@@ -69,6 +134,8 @@ public class Login extends AppCompatActivity {
     }
 
     public void goToSignUp(View view) {
+        Intent intent = new Intent(getBaseContext(), SignUp.class);
 
+        startActivity(intent);
     }
 }
