77201a9938471ce47be51daabed8b9ba3c3e36e5
Daniel Antonio Nolasco Alvarado <dnieln7@gmail.com>
2020-08-01 16:30:39 -0500

    added contact feature to about fragment

--- app/src/main/java/com/dnieln7/roadwatchman/ui/app/pages/about/About.java
+++ app/src/main/java/com/dnieln7/roadwatchman/ui/app/pages/about/About.java
@@ -1,5 +1,7 @@
 package com.dnieln7.roadwatchman.ui.app.pages.about;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -21,6 +23,21 @@ public class About extends Fragment {
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_about, container, false);
+        View root = inflater.inflate(R.layout.fragment_about, container, false);
+
+        root.findViewById(R.id.about_gmail).setOnClickListener(v -> openEmail());
+        root.findViewById(R.id.about_github).setOnClickListener(v -> goToGithub());
+
+        return root;
+    }
+
+    private void openEmail() {
+        Intent gmailIntent = new Intent(Intent.ACTION_SENDTO, Uri.parse("mailto:" + "dnieln7@gmail.com"));
+        startActivity(Intent.createChooser(gmailIntent, ""));
+    }
+
+    private void goToGithub() {
+        Intent githubIntent = new Intent(Intent.ACTION_VIEW, Uri.parse("https://github.com/dnieln7"));
+        startActivity(Intent.createChooser(githubIntent, ""));
     }
 }
